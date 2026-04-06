e997e7d17d358cd0acbbf047e5aa84b98eb28c0a
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-06 11:45:12 -0300

    Estudo prático sobre var

---
+++ 08-javascript/01-variaveis-e-constantes/01-var/script.js
@@ -0,0 +1,12 @@
+// Declara uma variável sem valor.
+var user
+
+console.log(user)
+
+// Declara uma variável com valor.
+var email = "alissonfreitas031@gmail.com"
+console.log(email)
+
+// Substitui o valor da variável.
+email = "joao@gmail.com" // Atribuindo um novo valor.
+console.log(email)
