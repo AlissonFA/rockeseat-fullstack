74321cdc0d7e3cb22c38fa22017e168c383969db
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-06 11:59:29 -0300

    Estudo prático de let

---
+++ 08-javascript/01-variaveis-e-constantes/03-let/script.js
@@ -0,0 +1,13 @@
+// Declara uma variável sem valor.
+let user
+console.log("Alisson Freitas")
+
+// Let não permite declarar duas variáveis com mesmo nome.
+// let user
+
+// Declara uma variável com valor.
+let email = "alisson@gmail.com"
+console.log(email)
+
+email = "lucas@gmail.com"
+console.log(email)
