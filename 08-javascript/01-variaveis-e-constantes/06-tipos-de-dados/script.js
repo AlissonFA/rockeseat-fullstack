8480b5997abc88d153722fe74cf075e3056f70c3
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-10 10:17:49 -0300

    Estudo prático de tipos de dados

---
+++ 08-javascript/01-variaveis-e-constantes/06-tipos-de-dados/script.js
@@ -0,0 +1,27 @@
+// JavaScript é dinâmico, o tipo da variável é definida dinamicamente pelo tipo do valor (não é necessário declarar o tipo).
+
+// Tipos Primitivos (disponiveis na própria linguagem)
+// String (texto)
+let username = "Alisson"
+console.log(username)
+console.log(typeof username) // typeof mostra o tipo
+
+console.log("Uma string com aspas")
+console.log('Uma string com apóstrofo (aspas simples)')
+
+// Quanto utilizar uma ou outra (aspas)?
+console.log('Uma string com "aspas duplas" dentro de simples.')
+
+console.log("Uma string com 'aspas simples' dentro de duplas.")
+
+console.log(`
+  Uma string com acento grave permite escrever múltiplas linhas.
+`)
+
+// Number (valor)
+
+// Boolean (verdadeiro ou falso)
+
+// Null (conteúdo vazio)
+
+// Undefined (JS atribui automaticamente para uma variável que não valor)
