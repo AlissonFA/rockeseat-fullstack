6f7003edeadb76feae7a470fccd8129bcf719516
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-13 09:54:19 -0300

    Estudo prático de operadores lógicos "igual a" e "diferente de"

---
+++ 08-javascript/02-expressoes-e-operadores/04-igual-e-diferente/script.js
@@ -0,0 +1,14 @@
+let one = 1
+let two = 2
+
+// == igual a (comparação)
+console.log("------------IGUAL A------------")
+console.log(one == two)
+console.log(one == 1)
+console.log(one == "1") // Ele olha para o conteúdo e não o tipo, então isso vai dar verdadeiro mesmo sem ser uma string.
+
+// != diferente de
+console.log("------------DIFERENTE DE------------")
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1")
