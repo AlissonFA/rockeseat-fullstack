d3875957453def6991b66fd45c3f1fe7a6be868d
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-13 10:52:27 -0300

    Estudo prático de maior, menor e igual

---
+++ 08-javascript/02-expressoes-e-operadores/05-maior-menor-igual/script.js
@@ -0,0 +1,17 @@
+let balance = 500 // Saldo da conta
+let payment = 120 // Valor do pagamento a ser feito
+// Queremos verificar se o saldo é suficiente pra pagar ou não
+
+// > Maior que
+console.log(balance > payment) // Saldo maior que o pagamento?
+
+// < Menor que
+console.log(balance < payment) // Saldo menor que o pagamento?
+
+// Maior ou igual
+balance = 120
+console.log(balance >= payment)
+
+// Menor ou igual
+balance = 500
+console.log(balance <= payment)
