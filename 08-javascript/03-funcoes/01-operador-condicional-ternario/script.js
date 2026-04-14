24e0ae993313277c532d512fe21ceb8c5e758a26
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-14 11:43:10 -0300

    Estudo prático do operador condicional ternário

---
+++ 08-javascript/03-funcoes/01-operador-condicional-ternario/script.js
@@ -0,0 +1,9 @@
+// Teste para saber se pode dirigir
+
+let age = 16
+console.log(age >= 18 // condição
+  ? "Você pode dirigir!" // se verdadeiro
+  : "Você não pode dirigir!" // se falso
+)
+
+console.log(age < 18 ? "Você não pode dirigir!" : "Você pode dirigir!")
