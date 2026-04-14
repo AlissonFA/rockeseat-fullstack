560336524a7a1c1c4a374d01dad23d6f463013b0
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-14 11:58:18 -0300

    Estudo prático do if

---
+++ 08-javascript/03-funcoes/03-if/script.js
@@ -0,0 +1,11 @@
+// If (SE)
+// Mensagem de bom dia se a hora for menor ou igual a 12
+let hour = 11
+
+if (hour <= 12) {
+  console.log("Bom dia!")
+}
+
+if (hour <= 12) 
+  console.log("Bom dia!")
+  console.log("Seja bem vindo!")
