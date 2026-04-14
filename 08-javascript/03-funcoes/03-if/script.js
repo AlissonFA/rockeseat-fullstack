b7c86215cbdbcc57d9881e4850729de4e81498db
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-14 12:02:40 -0300

    Estudo prático do if else

--- 08-javascript/03-funcoes/03-if/script.js
+++ 08-javascript/03-funcoes/03-if/script.js
@@ -1,5 +1,6 @@
 // If (SE)
 // Mensagem de bom dia se a hora for menor ou igual a 12
+  console.log("### IF ###")
 let hour = 11
 
 if (hour <= 12) {
@@ -9,3 +10,13 @@ if (hour <= 12) {
 if (hour <= 12) 
   console.log("Bom dia!")
   console.log("Seja bem vindo!")
+
+// If Else (Se não)
+console.log("### IF ELSE ###")
+let age = 17
+
+if (age < 18) {
+  console.log("Você não pode dirigir!")
+} else {
+  console.log("Você pode dirigir!")
+}
