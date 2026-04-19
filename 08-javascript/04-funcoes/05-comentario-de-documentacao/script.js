506e06eb43aaec50ba776fc66e76190fabc28a2b
Alisson Freitas <alissonfreitas031@gmail.com>
2026-04-19 11:09:29 -0300

    Comentário de Documentação

---
+++ 08-javascript/04-funcoes/05-comentario-de-documentacao/script.js
@@ -0,0 +1,14 @@
+/**
+ * Authenticates the user
+ * 
+ * @param {String} email user email.
+ * @param {String} password more than 6 characters.
+ * @returns {Number} user id.
+ */
+function signIn(email, password){
+  // Fluxo de autenticação do usuário.
+
+  return 7
+}
+
+signIn("alisson@gmail.com", "12345678")
