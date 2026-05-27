162c73777b5c3113f2a1fa3bb4f050a0fa431d91
Alisson Freitas <alissonfreitas031@gmail.com>
2026-05-27 10:26:23 -0300

    Criando meu próprio hook

--- 10-react/classroom/src/App.tsx
+++ 10-react/classroom/src/App.tsx
@@ -1,13 +1,16 @@
-import "./global.css"
+import "./global.css";
 
 import { Button } from "./components/button";
+import { useMessage } from "./hooks/useMessage";
 
 import styles from "./app.module.css";
 
 export function App() {
+  const { show } = useMessage({ name: "Alisson", age: 18 });
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" />
+      <Button name="Adicionar" onClick={() => show("Mensagem personalizada")} />
       <span>0</span>
       <Button name="Remover" />
     </div>
