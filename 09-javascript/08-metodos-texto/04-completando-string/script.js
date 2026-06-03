125474b33f2f0101e1e551a19029e18ca085c45c
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-03 00:51:32 -0300

    Modificando string

---
+++ 09-javascript/08-metodos-texto/04-completando-string/script.js
@@ -0,0 +1,13 @@
+const creditCard = 1234567812344928
+// console.log(String(creditCard).length)
+
+// pega os 4 últimos dígitos
+const lastDigits = String(creditCard).slice(-4)
+// console.log(lastDigits)
+
+// O padStart preenche a string do início
+const maskedNumber = lastDigits.padStart(String(creditCard).length, "X")
+
+console.log(maskedNumber)
+
+// O padEnd preenche a string no final
