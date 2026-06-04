fc8f3cddfe7834eed60ca972517004f49f9cf606
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-04 20:58:10 -0300

    Criando e acessando um array sem construtor

---
+++ 09-javascript/09-arrays/02-criando-sem-construtor/script.js
@@ -0,0 +1,15 @@
+// Criando e acessando um Array sem construtor
+let fruits = ["Apple", "Banana", "Orange", "Watermelon"];
+console.log(fruits)
+
+// Quantidade de itens do Array
+console.log(fruits.length)
+
+// Acessando o item pelo índice
+console.log(fruits[1])
+
+// Tentando acessar um índice que não existe
+console.log(fruits[7])
+
+// Obtém dinamicamente o último item
+console.log(fruits[fruits.length - 1])
