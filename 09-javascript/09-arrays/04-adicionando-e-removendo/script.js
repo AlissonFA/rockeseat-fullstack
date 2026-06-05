bdd79af6dc5a6c03f775f5efab8480c9b4a93be8
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-04 21:10:23 -0300

    Adicionando e removendo um item do array

---
+++ 09-javascript/09-arrays/04-adicionando-e-removendo/script.js
@@ -0,0 +1,17 @@
+let users = [];
+
+// Adiciona um item no final do Array
+users.push("Alisson");
+users.push("Rodrigo");
+users.push("Gabriel");
+
+// Adiciona um item no início do Array
+users.unshift("Ana");
+
+// Remove um item do início do Array
+users.shift()
+
+// Remove um item do final do Array
+users.pop()
+
+console.log(users);
