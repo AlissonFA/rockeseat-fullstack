d582b93d19b32fb850e5938c38d7ec28641d1b01
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-08 14:03:12 -0300

    Modificando uma data e uma hora

---
+++ 09-javascript/11-data-e-hora/04-modificando/script.js
@@ -0,0 +1,21 @@
+let date = new Date("Jun 8, 2026 14:00:00")
+
+// Modificar o ano.
+date.setFullYear(2030)
+
+// Modificar o mês (começa com 0).
+date.setMonth(8)
+
+// Modificar o dia.
+date.setDate(10)
+
+// Modificar a hora.
+date.setHours(18)
+
+// Modificar o minuto.
+date.setMinutes(45)
+
+// Modificar o segundo.
+date.setSeconds(30)
+
+console.log(date)
