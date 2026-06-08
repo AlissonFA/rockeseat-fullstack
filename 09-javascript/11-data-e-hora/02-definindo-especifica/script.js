d62121644c316ed2883f7d98392f88244fe4470b
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-08 12:37:11 -0300

    definindo uma data e hora específica

---
+++ 09-javascript/11-data-e-hora/02-definindo-especifica/script.js
@@ -0,0 +1,10 @@
+// Define com ano, mês (0-11), dia.
+console.log(new Date(2026, 5, 8))
+
+// Define ano, mês (0-11), dia, hora, minuto e segundo.
+console.log(new Date(2026, 6, 8, 12, 33, 0))
+
+// Definindo data e hora com String.
+console.log(new Date("2026-06-08T12:35:00"))
+
+console.log(new Date("Jun 8, 2026, 12:36:00"))
