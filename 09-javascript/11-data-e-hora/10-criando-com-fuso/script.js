d16a0cb26504b55fea6b3fd2deeb3c779b445b8b
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-10 10:00:53 -0300

    Criando uma data e hora com fuso horário

---
+++ 09-javascript/11-data-e-hora/10-criando-com-fuso/script.js
@@ -0,0 +1,5 @@
+const dateWithTimezone = new Date("2026-06-10T12:58:43.095+03:00")
+// console.log(dateWithTimezone.toISOString())
+
+console.log(new Date().toLocaleString())
+console.log(dateWithTimezone.toLocaleString())
