e085def8ea556ed8964c4032f3cbb7fe40d47b34
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-16 10:01:54 -0300

    Adicionando métodos nas classes

---
+++ 09-javascript/12-classes/03-metodos/script.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  // Não precisa colocar function, apenas colocar direto.
+  sendEmail() {
+    console.log("E-mail enviado para", this.name, "no endereço eletrônico", this.email);
+  }
+}
+
+const user = new User("Alisson", "alisson@gmail.com");
+user.sendEmail();
