c53fa2d0447fde9483242f15daaed7c6b5ed1f20
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-16 09:51:22 -0300

    Criando uma classe com um método construtor

---
+++ 09-javascript/12-classes/01-criando/script.js
@@ -0,0 +1,10 @@
+// class MyClass {}
+class Person {
+  // Executa automaticamente sempre que a classe for instanciada
+  constructor(name){
+    // console.log("Classe instanciada...")
+    console.log("Olá", name)
+  }
+}
+
+const person = new Person("Alisson")
