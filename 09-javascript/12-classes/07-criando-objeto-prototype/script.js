7b1956e7c5bf80029c45a89f04b47143d93fe821
Alisson Freitas <alissonfreitas031@gmail.com>
2026-06-16 10:36:43 -0300

    Criando um objeto prototype

---
+++ 09-javascript/12-classes/07-criando-objeto-prototype/script.js
@@ -0,0 +1,13 @@
+const address = {
+  city: "Aracaju",
+  country: "Brazil",
+};
+
+console.log(address);
+
+const users = ["Alisson", "Rodrigo", "João", "Maria"];
+
+console.log(users);
+
+const userName = "Alisson F";
+console.log(userName.__proto__);
